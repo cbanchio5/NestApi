5dda30922b6dd03d1cb1b94b84ce8f09b7f1383a
cesar banchio <cbanchio5@gmail.com>
2023-06-14 11:13:43 +0200

    changed schemas

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -8,7 +10,25 @@ export class AuthService {
     return { msg: 'I have signed in' };
   }
 
-  signup() {
+  async signup(dto: AuthDto) {
+    //generate the password hash
+
+    const hash = await argon.hash(dto.password);
+
+    //save the new user in db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    delete user.hash;
+
+    //return the saved user
+
+    return user;
+
     return { msg: 'I have signup' };
   }
 }
