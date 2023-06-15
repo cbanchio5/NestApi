21311376f20607bfbab7529999476fc669dfbc7a
cesar banchio <cbanchio5@gmail.com>
2023-06-15 10:16:29 +0200

    added signin method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
@@ -6,29 +6,62 @@ import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return { msg: 'I have signed in' };
-  }
 
   async signup(dto: AuthDto) {
-    //generate the password hash
-
+    // generate the password hash
     const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    //save the new user in db
-    const user = await this.prisma.user.create({
-      data: {
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+
+
+
+      //if (error instanceof PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+     // }
+      //throw error;
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/adjacent-overload-signatures
+  async signin(dto: AuthDto) {
+    //find user
+
+    const user = await this.prisma.user.findUnique({
+      where: {
         email: dto.email,
-        hash,
       },
     });
 
-    delete user.hash;
+    //if user does not exists throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
 
-    //return the saved user
+    //compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    //if password incorrect throw exception
+    if(!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    //send back user
+    delete user.hash;
 
     return user;
 
-    return { msg: 'I have signup' };
+
+
   }
 }
